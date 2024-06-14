355ba8cdfdcd83f907810ea7c0ca5a189268e1e1
Jewel <jewelsunnyk@gmail.com>
2024-06-14 10:05:37 -0500

    Maded changes to flashcard.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useStatae} from 'react';
+import React, {useState} from 'react';
 import {useQuery, useMutation, from} from '@apollo/client';
 import { GET_FLASHCARDS } from '../utils/queries';
 import { SUBMIT_ANSWER } from '../utils/mutations';
@@ -6,7 +6,17 @@ import { SUBMIT_ANSWER } from '../utils/mutations';
 const Home = () => {
   const {loading, error, data, refetch}= useQuery(GET_FLASHCARDS);
   const[submitAnswer]=useMutation(SUBMIT_ANSWER)
+  const [currentCard, setCurrentCard]= useState(null);
+  const[userAnswer, setUserAnswer]= useState('');
+  const[isFlipped, setIsFlipped]= useState(false)
   
+  const flipCard=() =>{
+    setIsFlipped(!isFlipped)
+  }
+
+  const handleNextCard=()=>{
+    
+  }
 
   return (
     <main>
