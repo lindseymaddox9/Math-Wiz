8b25361490a910bdd666ab8d8bc8528b174daec6
lindseymaddox9 <153481027+lindseymaddox9@users.noreply.github.com>
2024-06-14 08:14:27 -0500

    styled the header footer and created flashcards that flip created new files

---
+++ client/src/components/Card/index.jsx
@@ -0,0 +1,10 @@
+import "./style.css"
+function FlashCard({children}){
+return(
+    <div className="flash-card" style={{backGroundImage:"../../assets/images/gold_wizard_hat_720.png"}}>
+       {children}
+    </div>
+)
+}
+
+export default FlashCard
